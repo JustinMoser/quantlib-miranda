4971e1a6699f2bec0472cf8dcc4d8d1db5ef8458
Justin Moser <justin@baysickdesign.com>
2014-07-07 14:39:30 +0100

    removed deleted files

--- pricing.m
+++
@@ -1,8 +0,0 @@
-future_value::num -> num -> num -> num -> num
-future_value 0 r t m = 0
-future_value p 0 t m = p
-future_value p r 0 m = p
-future_value p r t 0 = p
-future_value p r t m = ((1+(r/m))^(t*m))*p
-
-||current_value::num -> num -> num -> num
